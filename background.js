35aafc8e9fb4082a64966ce74ca9fef843cf8496
Manu Cornet <m@ma.nu>
2022-08-05 09:19:40 +0000

    Need to expand messages before we can start downloading them

--- background.js
+++ background.js
@@ -18,38 +18,69 @@ async function downloadMessagesFromConversationView(tab) {
         };
 
         const delay = (t, v) => {
-           return new Promise(function(resolve) {
-               setTimeout(resolve.bind(null, v), t)
-           });
-        }
-
-        const moreButtons = document.querySelectorAll('[aria-label="More"]');
-        if (!moreButtons) {
-          console.log('Could not find the more button');
-          return;
-        }
-
-        console.log('Found more buttons: ', moreButtons);
-        const downloadMessagesPromises = [];
-
-        // There is one more button at the top of the page (we don't want that
-        // one), and then one per message. The only way I've seen to distinguish
-        // them so far is that the parent of the first one is a <div> and a
-        // <td> for the others.
-        for (const moreButton of moreButtons) {
-          console.log(moreButton.parentNode.tagName);
-          if (moreButton.parentNode.tagName.toLowerCase() === 'div') {
-            // This is the top more button, we don't want that one.
-            console.log('Skipping top "more" button');
-            continue;
-          }
-          console.log('Found more button on message', moreButton);
-          const downloadThisMessagePromise = new Promise((resolve, reject) => {
-            moreButton.dispatchEvent(generateEvent('click'));
-            return delay(2000).then(() => {
-              console.log('After timeout');
-            });
+          return new Promise(function(resolve) {
+            setTimeout(resolve.bind(null, v), t)
           });
+        };
+
+        // Some of the messages may be collapsed
+        const expandAllButton = document.querySelector('[aria-label="Expand all"]');
+        console.log('Expand all button? ', expandAllButton);
+        const expandPromise = expandAllButton ? new Promise((resolve, reject) => {
+          expandAllButton.dispatchEvent(generateEvent('click'));
+          return delay(500).then(() => {
+            resolve();
+          });
+        }) : Promise.resolve();
+
+        return expandPromise.then(() => {
+          const moreButtons = document.querySelectorAll('[aria-label="More"]');
+          if (!moreButtons) {
+            console.log('Could not find the more button');
+            return;
+          }
+          console.log('Found more buttons: ', moreButtons);
+
+          const downloadMessagesPromises = [];
+
+          // There is one more button at the top of the page (we don't want that
+          // one), and then one per message. The only way I've seen to distinguish
+          // them so far is that the parent of the first one is a <div> and a
+          // <td> for the others.
+          for (const moreButton of moreButtons) {
+            console.log(moreButton.parentNode.tagName);
+            if (moreButton.parentNode.tagName.toLowerCase() === 'div') {
+              // This is the top more button, we don't want that one.
+              console.log('Skipping top "more" button');
+              continue;
+            }
+            console.log('Found more button on message', moreButton);
+            const downloadThisMessagePromise = new Promise((resolve, reject) => {
+              return delay(2000).then(() => {
+                console.log('After timeout, clicking...');
+                moreButton.dispatchEvent(generateEvent('click'));
+              });
+            });
+            downloadMessagesPromises.push(downloadMessagesPromise);
+          }
+
+          // Now we have all the promises, let's run them in sequence.
+          return downloadMessagesPromises.reduce(
+            (promiseChain, currentTask) => {
+              return promiseChain.then(chainResults => currentTask.then(
+                (currentResult) => [...chainResults, currentResult])
+              );
+            },
+            Promise.resolve([]).then((arrayOfResults) => {
+              console.log('We got results: ', arrayOfResults);
+            })
+          );
+        });
+      }
+  });
+}
+
+
           // const i = document.evaluate('//div[text()="Download message"]',
               // document, null, XPathResult.ANY_TYPE, null );
           // const downloadButton = i.iterateNext();
@@ -73,21 +104,7 @@ async function downloadMessagesFromConversationView(tab) {
           // console.log(preciseEvent);
           // currentElement.dispatchEvent(preciseEvent);
           // downloadButton.dispatchEvent(generateEvent('mousedown'));
-        }
-        // Now we have all the promises, let's run them in sequence.
-        return downloadMessagesPromises.reduce(
-          (promiseChain, currentTask) => {
-            return promiseChain.then(chainResults => currentTask.then(
-              (currentResult) => [...chainResults, currentResult])
-            );
-          },
-          Promise.resolve([]).then((arrayOfResults) => {
-            console.log('We got results: ', arrayOfResults);
-          })
-        );
-      },
-  });
-}
+
 
 chrome.action.onClicked.addListener((tab) => {
   downloadMessagesFromConversationView(tab);
